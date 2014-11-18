0e91fa67ef0f63b4947df0adc8e5d158880063f4
neoatlantis <rijndael@yeah.net>
2014-11-18 03:07:06 +0100

    save

--- plot.py
+++ plot.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python
+"""
+NeoAtlantis MTSAT-2 Data Plotter
+Copyright (C) 2014 NeoAtlantis 
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import math
 
@@ -335,11 +351,17 @@ def envLabel(text):
     envDraw.text((envL, envT), text, font=font, fill="black")
     envT += textH * 1.5
 
+
+
+timestamp = time[0:4] + '-' + time[4:6] + '-' + time[6:8] + ' '
+timestamp += time[8:10] + ':' + time[10:12] + ' '
+timestamp += 'UTC'
+
 text = ("""
 NeoAtlantis MTSAT-2 IR Data Plotter
 ===================================
 
-Data Time: %s
+Timestamp: %s
 IR: %d
 
 
@@ -368,7 +390,7 @@ You should have received a copy of
 the GNU General Public License
 along with this program.  If not,
 see <http://www.gnu.org/licenses/>.
-""" % (time, ir)).strip().split('\n')
+""" % (timestamp, ir)).strip().split('\n')
 for line in text:
     envLabel(line.strip())
 

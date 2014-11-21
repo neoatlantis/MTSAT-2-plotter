f417621e6ef59f4f870631abcc5ad762987cea07
neoatlantis <rijndael@yeah.net>
2014-11-21 18:12:17 +0100

    save

--- site/web/browser.js
+++ site/web/browser.js
@@ -1,10 +1,49 @@
 $(function(){
 //////////////////////////////////////////////////////////////////////////////
 
-//$.get('http://mtsat-2.neoatlantis.org/data/201411/log.txt', function(s){
-//});
+function validateDateFormat(s){
+    if(!/^[0-9]{8}$/.test(s)) return false;
+    var year = parseInt(s.slice(0,4), 10),
+        month = parseInt(s.slice(4,6), 10),
+        day = parseInt(s.slice(6, 8));
+    if(year < 2014) return false;
+    if(month < 1 or month > 12) return false;
+    if(day < 1) return false;
+
+    var dayMax = 0;
+    if(2 == month){
+        if( ((0 == year % 100)?(0 == year % 400):(0 == year % 4)) )
+            // is leap year
+            dayMax = 29;
+        else
+            dayMax = 28;
+    } else {
+        if(month <= 7)
+            dayMax = ((1 == month % 2)?31:30);
+        else
+            dayMax = ((1 == month % 2)?30:31);
+    };
+    if(day > dayMax) return false;
+    return [year, month, day]; 
+};
 
 
+
+
+
+/* initialize interface */
+
+function validateDateRange(){
+    var viewDateRangeStart = $('[name="view-date-range-start"]').text(),
+        viewDateRangeEnd = $('[name="view-date-range-end"]').text();
+    
+    if(!(
+        validateDateFormat(viewDateRangeStart) && 
+        validateDateFormat(viewDateRangeEnd))
+    )
+        return false;
+    
+};
 
 //////////////////////////////////////////////////////////////////////////////
 });

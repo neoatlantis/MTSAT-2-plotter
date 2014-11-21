bda0153a54634be574dafd5507dfc5c977171a8f
neoatlantis <rijndael@yeah.net>
2014-11-21 19:30:06 +0100

    save

--- site/web/browser.js
+++ site/web/browser.js
@@ -27,7 +27,60 @@ function validateDateFormat(s){
     return [year, month, day]; 
 };
 
+function compareDate(a, b){
+    var year1 = parseInt(a.slice(0,4), 10),
+        month1 = parseInt(a.slice(4,6), 10),
+        day1 = parseInt(a.slice(6, 8));
+    var year2 = parseInt(b.slice(0,4), 10),
+        month2 = parseInt(b.slice(4,6), 10),
+        day2 = parseInt(b.slice(6, 8));
 
+    var aBigger = false;
+    if(year1 > year2)
+        aBigger = true;
+    else if (year1 == year2)
+        if(month1 > month2)
+            aBigger = true;
+        else if(month1 == month2)
+            if(day1 > day2)
+                aBigger = true;
+
+    if(aBigger)
+        return [[year2, month2, day2], [year1, month1, day1]];
+    else
+        return [[year1, month1, day1], [year2, month2, day2]];
+    // returns the smaller, then the bigger
+};
+
+/* Load satellite data from given filename */
+var loadDataCache = {}
+var loadData = function(filename, callback){
+    if(!/^[0-9]{12}\.((IR[1-4])|VIS)\.(FULL|NORTH|SOUTH)\.png$/.test(filename))
+        return false;
+
+    var datemonth = filename.slice(0, 6),
+        url = 'http://mtsat-2.neoatlantis.org/data/'
+            + datemonth
+            + '/' + filename
+    ;
+
+    if(undefined !== loadDataCache[filename]){
+        
+    } else {
+        var img = new Image();
+        img.src = url;
+        img.onload = function(){
+            if(
+                !this.complete ||
+                typeof this.naturalWidth == "undefined" ||
+                this.naturalWidth == 0
+            )
+                loadDataCache[filename] = false;
+            else
+                loadDataCache[filename] = img;
+        };
+    };
+};
 
 
 
@@ -42,7 +95,6 @@ function validateDateRange(){
         validateDateFormat(viewDateRangeEnd))
     )
         return false;
-    
 };
 
 //////////////////////////////////////////////////////////////////////////////

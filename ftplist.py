fcf9ea0ed6d8e383e933d9e2ff8c848fe3bbd7e3
neoatlantis <rijndael@yeah.net>
2016-07-26 08:04:25 +0200

    A nearly standalone raw2pgm.py tool

--- ftplist.py
+++ ftplist.py
@@ -10,28 +10,31 @@ import re
 
 from ftplib import FTP
 
-if \
-    len(sys.argv) < 3 or\
-    not re.match('^[0-9]{6}$', sys.argv[1]) or\
-    not sys.argv[2] in ['EXT', 'SIR', 'TIR', 'VIS']:
-    
-    print "Usage: python ftplist.py <YYYYMM> <EXT|SIR|TIR|VIS>"
-    sys.exit(1)
-dateMonth = sys.argv[1]
-channel = sys.argv[2]
+def listHMWR8(dateMonth, channel):
+    ftp = FTP('hmwr829gr.cr.chiba-u.ac.jp')     # connect to host, default port
+    loginRet = ftp.login()                     # user anonymous, passwd anonymous
+    if loginRet.startswith('230'):
+        ftp.cwd('gridded/FD/V20151105/%s/%s' % (dateMonth, channel))
+        ftp.dir() #retrlines('LIST')
+    else:
+        sys.exit(1)
 
-ftp = FTP('hmwr829gr.cr.chiba-u.ac.jp')     # connect to host, default port
-loginRet = ftp.login()                     # user anonymous, passwd anonymous
-if loginRet.startswith('230'):
-    ftp.cwd('gridded/FD/V20151105/%s/%s' % (dateMonth, channel))
-    ftp.dir() #retrlines('LIST')
-else:
-    sys.exit(1)
-
-try:
-    ftp.quit()
-except:
     try:
-        ftp.close()
+        ftp.quit()
     except:
-        pass
+        try:
+            ftp.close()
+        except:
+            pass
+
+if __name__ == '__main__':
+    if \
+        len(sys.argv) < 3 or\
+        not re.match('^[0-9]{6}$', sys.argv[1]) or\
+        not sys.argv[2] in ['EXT', 'SIR', 'TIR', 'VIS']:
+        
+        print "Usage: python ftplist.py <YYYYMM> <EXT|SIR|TIR|VIS>"
+        sys.exit(1)
+    dateMonth = sys.argv[1]
+    channel = sys.argv[2]
+    listHMWR8(dateMonth, channel)
--- raw2pgm.py
+++ raw2pgm.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import os
 import sys
 import re
@@ -11,13 +12,131 @@ from convfunc import getConversionFunction
 
 ##############################################################################
 
-# Determine input and output file paths
+parser = argparse.ArgumentParser(\
+    description="""
+A multi-purpose tool for downloading and generating high-resolution satellite
+images from Himawari-8. Developed by NeoAtlantis.
+    """
+)
 
-if len(sys.argv) < 2:
-    print "Usage: python raw2pgm.py <INPUT FILE>"
+parser.add_argument(
+    'command',
+    choices=['list', 'download', 'draw'],
+    help="""What would you like to do with this tool. For `list`, the program
+    will connect to the FTP server located at <hmwr829gr.cr.chiba-u.ac.jp> and
+    list available tarballs for downloading. For `download` with given argument
+    on timestamp and channel, a tarball will be downloaded. For `draw`, specify
+    a downloaded tarball, this tool will generate a PGM grey scale picture
+    using its data.
+    """
+)
+
+parser.add_argument(
+    '--date',
+    action='store',
+    type=str,
+    required=False,
+    help="""Required and valid only when `list` is choosen. Otherwise ignored.
+    Give this option with a 6-digits value like 201606, to specify a month
+    from which all available tarballs will be listed. The date is UTC.
+    """
+)
+
+parser.add_argument(
+    '--timestamp',
+    action='store',
+    type=str,
+    required=False,
+    help="""Required when `download` is choosen. Specify a timestamp of the
+    data being transmitted. The timestamp must consist 12 digits in format of
+    YYYYmmDDHHMM, e.g. a 4-digits year, a 2-digits month, a 2-digits day, and
+    hour as well as minute in 2-digits each. Example: 201606010230. The
+    timestamp is UTC.
+    """
+)
+
+parser.add_argument(
+    '--channel',
+    action='store',
+    choices=[
+        'ext', 'vis', 'sir', 'tir',
+        'ext01', 'vis01', 'vis02', 'vis03', 'sir01', 'sir02', 'tir01', 'tir02',
+        'tir03', 'tir04', 'tir05', 'tir06', 'tir07', 'tir08', 'tir09', 'tir10',
+    ],
+    required=False,
+    help="""
+        Required when `download` or `list` is choosen.  Choose a channel. To
+        `list`, just one choice out of `ext`, `vis`, `sir`, `tir` is enough. To
+        `download`, you must also specify the channel ID, e.g. 01-10 depending
+        on channel choice.
+    """
+)
+
+parser.add_argument(
+    '--input',
+    action='store',
+    required=False,
+    help="""Required when `draw` is choosen. Give the input file name. Can be
+    either a `.geoss`(decompressed) or `.geoss.bz2`(compressed) file.
+    """
+)
+
+args = parser.parse_args()
+
+COMMAND = args.command
+
+DATE = args.date
+if 'list' == COMMAND:
+    try:
+        assert re.match('^[0-9]{4}(0[1-9]|10|11|12)$', DATE)
+    except:
+        print "Invalid --date specification. Must be YYYYMM format."
+        sys.exit(1)
+
+TIMESTAMP = args.timestamp
+if 'download' == COMMAND:
+    try:
+        assert re.match('^[0-9]{12}$', TIMESTAMP)
+    except:
+        print "Invalid --timestamp specification. Must be 12-digits."
+        sys.exit(1)
+
+CHANNELNAME, CHANNELID = '', -1 
+if args.channel:
+    CHANNELNAME = args.channel[:3].upper()
+    CHANNELID = int(args.channel[3:] or -1)
+    if COMMAND == 'download' and CHANNELID == -1:
+        print "You must specify the full channel representation, e.g. XXXYY"
+        sys.exit(1)
+elif COMMAND in ['download', 'list']:
+    print "You must also specify the channel. Use --channel argument."
     sys.exit(1)
 
-inputFile = sys.argv[1]
+inputFile = args.input
+
+##############################################################################
+
+# If used for listing or downloading
+
+if 'list' == COMMAND:
+    from ftplist import listHMWR8
+    listHMWR8(DATE, CHANNELNAME)
+    sys.exit()
+
+if 'download' == COMMAND:
+    filename = '%s.%s.%02d.fld.geoss.bz2' % (
+        TIMESTAMP, CHANNELNAME.lower(), CHANNELID, 
+    )
+    url = 'ftp://hmwr829gr.cr.chiba-u.ac.jp/gridded/FD/V20151105/%s/%s/%s' % (\
+        TIMESTAMP[:6], CHANNELNAME, filename
+    )
+    subprocess.call(['wget', url])
+    sys.exit()
+
+##############################################################################
+
+# Determine input and output file paths
+
 inputFilepath = os.path.realpath(os.path.dirname(inputFile))
 inputFilename = os.path.basename(inputFile)
 inputFile = os.path.join(inputFilepath, inputFilename)
